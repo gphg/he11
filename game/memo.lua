7fe17483a0291edea24d008f148046bab2796fd3
Garett PHG <suffering.train@yahoo.com>
2024-10-22 03:15:25 +0700

    Updated build script and library (git submodule)

--- game/memo.lua
+++ game/memo.lua
@@ -1,9 +1,1 @@
-return function (fn)
-    local cache = {}
-    return function (key)
-        if not cache[key] then
-            cache[key] = fn(key)
-        end
-        return cache[key]
-    end
-end
+return require 'lib.knife.memoize'
